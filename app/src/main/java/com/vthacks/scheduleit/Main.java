200bf49e6506369cf520fd768ad2eac70310d454
Matthewhacksu <47680934+Matthewhacksu@users.noreply.github.com>
2019-02-16 08:48:02 -0500

    added Schedule page and button to go to the schedule page

--- app/src/main/java/com/vthacks/scheduleit/Main.java
+++ app/src/main/java/com/vthacks/scheduleit/Main.java
@@ -1,7 +1,10 @@
 package com.vthacks.scheduleit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button scheduleBtn = (Button)findViewById(R.id.scheduleBtn);
+        scheduleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(),schedulePage.class);
+                startActivity(startIntent);
+
+            }
+        });
     }
 }
