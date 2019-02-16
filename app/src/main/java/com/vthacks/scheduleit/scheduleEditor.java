d656ded2ac431e7dec21eb9553d3f6405a80bd47
eahnj <ahnedwinj@gmail.com>
2019-02-16 13:21:29 -0500

    init firebase import and start for others

--- app/src/main/java/com/vthacks/scheduleit/scheduleEditor.java
+++ app/src/main/java/com/vthacks/scheduleit/scheduleEditor.java
@@ -4,9 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class scheduleEditor extends AppCompatActivity {
+
+    // Write a message to the database
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Schedule");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
